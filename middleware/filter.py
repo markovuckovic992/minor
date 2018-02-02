4e2075abea4cc4e1d978ca8e3f5c8537df2963a7
markovuckovic992 <markovuckovic992@yahoo.com>
2018-02-02 11:41:46 +0100

    'asd'

--- middleware/filter.py
+++ middleware/filter.py
@@ -23,4 +23,9 @@ class AllowIpMiddleware(object):
                     return response
                 return http.HttpResponseForbidden('<h1>Forbidden 1</h1>')
         except:
-            return http.HttpResponse(request.META.__dict__)
+            resp = {}
+            for key, value in request.META.keys():
+                resp.update({
+                    key: request.META[key]
+                })
+            return http.HttpResponse(resp)
